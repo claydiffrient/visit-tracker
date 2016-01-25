859871cae413201052a109354799d58da99e649d
Clay Diffrient <clay.diffrient@gmail.com>
2016-01-24 23:12:30 -0700

    Add enhanced navigation bar

--- src/web/containers/AppContainer.jsx
+++ src/web/containers/AppContainer.jsx
@@ -2,7 +2,8 @@ import { connect } from 'react-redux';
 import * as Actions from '../actions';
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Grid, Row, Col, Button, ButtonGroup} from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+import { Nav, NavItem, NavDropdown, Navbar, MenuItem } from 'react-bootstrap';
 
 function mapStateToProps (state) {
   return {
@@ -29,13 +30,27 @@ class App extends Component {
 
   render () {
     return (
-      <Grid>
-        <Row className='text-center'>
-          <Col sm={12}>
-            App Header Goes Here
+      <Grid fluid={true}>
+        <Navbar inverse fixedTop>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">Visit Tracker</a>
+              </Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                <NavItem eventKey={1} href="#">Last Visited</NavItem>
+                <NavItem eventKey={2} href="#">Visits</NavItem>
+                <NavItem eventKey={3} href="/people">People</NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        <Row>
+          <Col style={{marginTop: '70px'}} xs={12}>
+            {React.cloneElement(this.props.children, {...this.props})}
           </Col>
         </Row>
-        {React.cloneElement(this.props.children, {...this.props})}
       </Grid>
     );
   }
