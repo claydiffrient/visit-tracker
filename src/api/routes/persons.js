14d4e7761d1696c1259bb591d335f1b3709c242d
Clay Diffrient <clay.diffrient@gmail.com>
2016-03-01 22:47:04 -0700

    Add api documentation generation

--- src/api/routes/persons.js
+++ src/api/routes/persons.js
@@ -3,8 +3,30 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Person = mongoose.model('Person');
+const Visit = mongoose.model('Visit');
 
-/* GET Persons */
+/**
+ * @apiDefine Person
+ * @apiVersion 1.0.0
+ *
+ * @apiSuccess {Object[]}                 persons             List of persons
+ * @apiSuccess {String}                   persons.name        Name of the person
+ * @apiSuccess {String}                   persons.address     Address of the person
+ * @apiSuccess {String}                   persons.status      Status of the person
+ * @apiSuccess {ObjectId[]}               persons.visits      List of visit IDs for the person
+ * @apiSuccess {Date}                     persons.created_at  When record was created
+ * @apiSuccess {Date}                     persons.updated_at  When record was last updated
+ */
+
+/**
+ * @api {get} /persons Get all persons
+ * @apiName GetPersons
+ * @apiGroup Person
+ *
+ * @apiVersion 1.0.0
+ * @apiUse Person
+ *
+ */
 router.get('/', (req, res) => {
   Person.find({}, (err, persons) => {
     if (err) return res.status(500).send(err);
@@ -12,7 +34,15 @@ router.get('/', (req, res) => {
   });
 });
 
-/* POST Persons */
+/**
+ * @api {post} /persons Create a person
+ * @apiName CreatePerson
+ * @apiGroup Person
+ *
+ * @apiVersion 1.0.0
+ * @apiUse Person
+ *
+ */
 router.post('/', (req, res) => {
   const person = new Person(req.body);
 
@@ -24,7 +54,20 @@ router.post('/', (req, res) => {
   });
 });
 
-/* DELETE Person */
+/**
+ * @api {delete} /persons/:id Delete a person
+ * @apiName DeletePerson
+ * @apiGroup Person
+ *
+ * @apiVersion 1.0.0
+ *
+ * @apiParam  {Number}  id  Unique id for the person
+ *
+ * @apiSuccess    {Object}  deletedPerson          The person deleted
+ * @apiSuccess    {Person}  deletedPerson.person   The person object representing the deleted person
+ * @apiSuccess    {Boolean} deletedPerson.deleted  Indicates if the deletion was successful
+ *
+ */
 router.delete('/:id', (req, res) => {
   Person.findByIdAndRemove(req.params.id, (err, person) => {
     if (err) return res.status(500).send(err);
@@ -32,6 +75,24 @@ router.delete('/:id', (req, res) => {
       person,
       deleted: true
     });
+  });
+});
+
+/**
+ * @api {post} /persons/:id/visit Register a visit
+ * @apiName VisitPerson
+ * @apiGroup Person
+ *
+ * @apiVersion 1.0.0
+ *
+ * @apiParam  {Number}  id  Unique id for the person
+ *
+ *
+ */
+router.post('/:id/visit', (req, res) => {
+  Person.findById(req.params.id, (err, person) => {
+    if (err) return res.status(500).send(err);
+    const visit = new Visit()
   });
 });
 
--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
 
-/* GET Users */
+/**
+ * @api {get} /users Get all users
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiVersion 1.0.0
+ *
+ */
 router.get('/', (req, res) => {
   User.find({}, (err, users) => {
     if (err) res.status(500).send(err);
