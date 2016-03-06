232f8d6620cf8387f26688283f88e4fa1ec5f9ec
Clay Diffrient <clay.diffrient@gmail.com>
2016-03-05 18:18:07 -0700

    Remove uneeded commented out method

--- src/api/routes/visits.js
+++ src/api/routes/visits.js
@@ -22,34 +22,3 @@ router.get('/', auth, (req, res) => {
 
 
 module.exports = router;
-
-// /**
-//  * @api {post} /persons/:id/visit Register a visit
-//  * @apiName VisitPerson
-//  * @apiGroup Person
-//  *
-//  * @apiVersion 1.0.0
-//  *
-//  * @apiParam  {Number}  id              Unique id for the person
-//  *
-//  * @apiParam  {String}  notes           Notes about the visit
-//  * @apiParam  {Date}    date_visited    The date the visit occurred
-//  *
-//  */
-// router.post('/:id/visit', auth, (req, res) => {
-//   Person.findById(req.params.id, (err, person) => {
-//     if (err) return res.status(500).send(err);
-//     const visit = new Visit(req.body);
-
-//     visit.save((err, savedVisit) => {
-//       if (err) return res.status(500).send(err);
-
-//       person.visits.push(savedVisit._id);
-
-//       person.save(function (err, savedPerson) {
-//         if (err) return res.status(500).send(err);
-//         res.json(savedVisit);
-//       })
-//     });
-//   });
-// });
