410b16bcaef9784f44e789c9277bcdac7f12befa
Clay Diffrient <clay.diffrient@gmail.com>
2016-04-30 00:39:52 -0600

    Remove unnecessary compiled files

--- src/web/built/assets/bundle.js
+++
@@ -1,9 +0,0 @@
-webpackJsonp([0,1],[
-/* 0 */
-/***/ function(module, exports) {
-
-	console.log('Index');
-
-/***/ }
-]);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7O0FBQUEsUUFBTyxDQUFDLEdBQUcsQ0FBQyxPQUFPLENBQUMsQyIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSW5kZXgnKTtcblxuXG5cbi8qKiBXRUJQQUNLIEZPT1RFUiAqKlxuICoqIC4vaW5kZXguanNcbiAqKi8iXSwic291cmNlUm9vdCI6IiJ9
